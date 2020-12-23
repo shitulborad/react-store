e06dd621abc65608f88cb8273444ca7ba6221f19
Shitul Borad <shitulborad@gmail.com>
2020-12-23 10:48:31 +0530

    data change

--- src/data.js
+++ src/data.js
@@ -95,7 +95,7 @@ export const storeProducts = [
     id: 8,
     title: "Mactree 32",
     img: "img/item-8.jpeg",
-    price: 2,
+    price: 50,
     company: "Mactree",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
